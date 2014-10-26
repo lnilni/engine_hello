657a1eeaf4e894da77a7903feb1cdd2f3206416d
lni_T <developer.lni@gmail.com>
2014-10-27 04:38:41 +0900

    showアクションとルーティングを定義

--- app/controllers/engine_hello/hellos_controller.rb
+++ app/controllers/engine_hello/hellos_controller.rb
@@ -2,5 +2,8 @@ require_dependency "engine_hello/application_controller"
 
 module EngineHello
   class HellosController < ApplicationController
+    def show
+      render text: "Hello World"
+    end
   end
 end
